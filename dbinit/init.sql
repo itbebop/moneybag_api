3ac82c63d93c155fd6fb4bc6e63cbf448f5482ba
itbebop <it.bebop@gmail.com>
2024-11-20 11:16:31 +0900

    style: 오타수정

--- dbinit/init.sql
+++ dbinit/init.sql
@@ -8,11 +8,11 @@ CREATE TABLE users (
     uid             VARCHAR(255) NOT NULL,
     name            VARCHAR(255) NOT NULL,
     email           VARCHAR(255) NOT NULL,
-    user_type       VARCHAR(255) 'free',
+    user_type       VARCHAR(255) DEFAULT 'free',
     img_url         VARCHAR(255),
     first_pallete   VARCHAR(255),
     second_pallete  VARCHAR(255),
-    language        VARCHAR(255) 'ko',   
+    language        VARCHAR(255) DEFAULT 'ko',   
     create_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (uid),
     CONSTRAINT UQ_User_Email UNIQUE (email)
--- src/config/mariadb.config.js
+++ src/config/mariadb.config.js
@@ -7,7 +7,7 @@ const pool = mariadb.createPool({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DB_NAME,
+    database: process.env.DB_NAME,
     connectionLimit: process.env.DB_CONNECTION_LIMIT,
 });
 
