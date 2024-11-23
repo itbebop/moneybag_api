b060bd64c8df75dda4330c732db6c6bb1b499bfc
itbebop <it.bebop@gmail.com>
2024-11-23 20:32:45 +0900

    feat: user - userId로 변경 - isActivated, unActivatedAt 추가

--- dbinit/init.sql
+++ dbinit/init.sql
@@ -5,19 +5,21 @@ USE moneybagdb;
 
 -- USERS 테이블
 CREATE TABLE users (
-    uid             VARCHAR(255) NOT NULL PRIMARY KEY,
+    userId          INT AUTO_INCREMENT PRIMARY KEY,
     name            VARCHAR(255) NOT NULL,
     email           VARCHAR(255) NOT NULL UNIQUE,
-    userType       VARCHAR(255) DEFAULT 'free',
-    imgUrl         VARCHAR(255),
+    userType        VARCHAR(255) DEFAULT 'free',
+    imgUrl          VARCHAR(255),
     language        VARCHAR(255) DEFAULT 'ko',
-    createAt       TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    createAt        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    isActivated     BOOL DEFAULT TRUE,
+    unActivatedAt   TIMESTAMP NULL,
 );
 
 CREATE TABLE assets (
     assetId        INT AUTO_INCREMENT PRIMARY KEY,
     assetName      VARCHAR(255) NOT NULL,
-    currency        VARCHAR(255) DEFAULT 'KRW',
+    currency       VARCHAR(255) DEFAULT 'KRW',
     createAt       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updateAt       TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
@@ -54,23 +56,23 @@ CREATE TABLE user_pallete (
     colorId        INT AUTO_INCREMENT PRIMARY KEY,
     colorStr       VARCHAR(255) NOT NULL,
     colorOrder     INT NOT NULL,
-    uid             VARCHAR(255) NOT NULL,
-    CONSTRAINT fk_user_pallete FOREIGN KEY (uid) REFERENCES users(uid) ON DELETE CASCADE
+    userId            INT NOT NULL,
+    CONSTRAINT fk_user_pallete FOREIGN KEY (userId) REFERENCES users(userId) ON DELETE CASCADE
 );
 
 CREATE TABLE user_user (
     id              INT AUTO_INCREMENT PRIMARY KEY,
-    uid1            VARCHAR(255) NOT NULL,
-    uid2            VARCHAR(255) NOT NULL,
-    createAt       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_user_user1 FOREIGN KEY (uid1) REFERENCES users(uid) ON DELETE CASCADE,
-    CONSTRAINT fk_user_user2 FOREIGN KEY (uid2) REFERENCES users(uid) ON DELETE CASCADE
+    userId1            INT NOT NULL,
+    userId2            INT NOT NULL,
+    createAt        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user_user1 FOREIGN KEY (userId1) REFERENCES users(userId) ON DELETE CASCADE,
+    CONSTRAINT fk_user_user2 FOREIGN KEY (userId2) REFERENCES users(userId) ON DELETE CASCADE
 );
 
 CREATE TABLE user_asset (
     id              INT AUTO_INCREMENT PRIMARY KEY,
-    uid             VARCHAR(255) NOT NULL,
-    assetId        INT NOT NULL,
-    CONSTRAINT fk_user_asset1 FOREIGN KEY (uid) REFERENCES users(uid) ON DELETE CASCADE,
+    userId             INT NOT NULL,
+    assetId         INT NOT NULL,
+    CONSTRAINT fk_user_asset1 FOREIGN KEY (userId) REFERENCES users(userId) ON DELETE CASCADE,
     CONSTRAINT fk_user_asset2 FOREIGN KEY (assetId) REFERENCES assets(assetId) ON DELETE CASCADE
 );
