c34cb754868556dc93bd31591807d3f327bd4285
itbebop <it.bebop@gmail.com>
2024-11-25 19:28:54 +0900

    feat: init sql 수정 - user간, asset별 pallete 관계 재설정 등

--- dbinit/init.sql
+++ dbinit/init.sql
@@ -2,8 +2,6 @@ CREATE DATABASE IF NOT EXISTS moneybagdb;
 
 USE moneybagdb;
 
-
--- USERS 테이블
 CREATE TABLE users (
     userId          INT AUTO_INCREMENT PRIMARY KEY,
     name            VARCHAR(255) NOT NULL,
@@ -13,58 +11,61 @@ CREATE TABLE users (
     imgUrl          VARCHAR(255),
     language        VARCHAR(255) DEFAULT 'ko',
     createAt        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    isDeleted       BOOL DEFAULT TRUE,
-    deletedAt       TIMESTAMP NULL,
+    isDeleted       BOOL DEFAULT FALSE,
+    deletedAt       TIMESTAMP
+);
+
+CREATE TABLE user_pallete (
+    colorId        INT AUTO_INCREMENT PRIMARY KEY,
+    colorStr       VARCHAR(20),
+    colorOrder     INT NOT NULL,
+    userId         INT NOT NULL,
+    CONSTRAINT fk_user_pallete FOREIGN KEY (userId) REFERENCES users(userId) ON DELETE CASCADE
 );
 
 CREATE TABLE assets (
     assetId        INT AUTO_INCREMENT PRIMARY KEY,
     assetName      VARCHAR(255) NOT NULL,
-    currency       VARCHAR(255) DEFAULT 'KRW',
+    currency       VARCHAR(3) DEFAULT 'KRW',
     createAt       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updateAt       TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
-CREATE TABLE asset_color (
-    colorId        INT AUTO_INCREMENT PRIMARY KEY,
-    firstColor     VARCHAR(255) NOT NULL,
-    secondColor    VARCHAR(255) NOT NULL,
-    assetId        INT NOT NULL,
-    CONSTRAINT fk_asset FOREIGN KEY (assetId) REFERENCES assets(assetId) ON DELETE CASCADE
+    updateAt       TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    isActivated    BOOL DEFAULT TRUE,
+    firstColor     INT NOT NULL,
+    secondColor    INT NOT NULL,
+    CONSTRAINT fk_asset_firstColor FOREIGN KEY (firstColor) REFERENCES user_pallete(colorId) ON DELETE CASCADE,
+    CONSTRAINT fk_asset_secondColor FOREIGN KEY (secondColor) REFERENCES user_pallete(colorId) ON DELETE CASCADE
 );
 
 CREATE TABLE category (
     categoryId     INT AUTO_INCREMENT PRIMARY KEY,
-    name            VARCHAR(255) NOT NULL,
+    categoryName   VARCHAR(255) NOT NULL,
     iconKey        VARCHAR(255) DEFAULT NULL,
     assetType      VARCHAR(255) NOT NULL,
-    level           INT DEFAULT 1
+    level          INT DEFAULT 1
 );
 
 CREATE TABLE transactions (
     transactionId      INT AUTO_INCREMENT PRIMARY KEY,
-    memo                VARCHAR(255) NOT NULL,
-    currency            VARCHAR(255) DEFAULT 'KRW',
+    memo               VARCHAR(255) NOT NULL,
+    amount             DOUBLE NOT NULL,
+    currency           VARCHAR(3) DEFAULT 'KRW',
     createAt           TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updateAt           TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    imgUrl             VARCHAR(255), 
     categoryId         INT NOT NULL,
-    subCategoryId     INT,
+    subCategoryId      INT,
+    userId             INT NOT NULL,
+    assetId            INT NOT NULL,
     CONSTRAINT fk_category FOREIGN KEY (categoryId) REFERENCES category(categoryId) ON DELETE CASCADE,
-    CONSTRAINT fk_sub_category FOREIGN KEY (subCategoryId) REFERENCES category(categoryId) ON DELETE SET NULL
-);
-
-CREATE TABLE user_pallete (
-    colorId        INT AUTO_INCREMENT PRIMARY KEY,
-    colorStr       VARCHAR(255) NOT NULL,
-    colorOrder     INT NOT NULL,
-    userId            INT NOT NULL,
-    CONSTRAINT fk_user_pallete FOREIGN KEY (userId) REFERENCES users(userId) ON DELETE CASCADE
+    CONSTRAINT fk_sub_category FOREIGN KEY (subCategoryId) REFERENCES category(categoryId) ON DELETE SET NULL,
+    CONSTRAINT fk_transaction_user FOREIGN KEY (userId) REFERENCES users(userId) ON DELETE CASCADE,
+    CONSTRAINT fk_transaction_asset FOREIGN KEY (assetId) REFERENCES assets(assetId) ON DELETE CASCADE
 );
 
 CREATE TABLE user_user (
     id              INT AUTO_INCREMENT PRIMARY KEY,
-    userId1            INT NOT NULL,
-    userId2            INT NOT NULL,
+    userId1         INT NOT NULL,
+    userId2         INT NOT NULL,
     createAt        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_user_user1 FOREIGN KEY (userId1) REFERENCES users(userId) ON DELETE CASCADE,
     CONSTRAINT fk_user_user2 FOREIGN KEY (userId2) REFERENCES users(userId) ON DELETE CASCADE
@@ -72,8 +73,13 @@ CREATE TABLE user_user (
 
 CREATE TABLE user_asset (
     id              INT AUTO_INCREMENT PRIMARY KEY,
-    userId             INT NOT NULL,
+    userId          INT NOT NULL,
     assetId         INT NOT NULL,
-    CONSTRAINT fk_user_asset1 FOREIGN KEY (userId) REFERENCES users(userId) ON DELETE CASCADE,
-    CONSTRAINT fk_user_asset2 FOREIGN KEY (assetId) REFERENCES assets(assetId) ON DELETE CASCADE
+    firstColor      INT NOT NULL,
+    secondColor     INT NOT NULL,
+    CONSTRAINT fk_user_asset_user FOREIGN KEY (userId) REFERENCES users(userId) ON DELETE CASCADE,
+    CONSTRAINT fk_user_asset_asset FOREIGN KEY (assetId) REFERENCES assets(assetId) ON DELETE CASCADE,
+    CONSTRAINT fk_user_asset_firstColor FOREIGN KEY (firstColor) REFERENCES user_pallete(colorId) ON DELETE CASCADE,
+    CONSTRAINT fk_user_asset_secondColor FOREIGN KEY (secondColor) REFERENCES user_pallete(colorId) ON DELETE CASCADE
 );
+
